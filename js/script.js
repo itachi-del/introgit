17de1ae89dbd57f138410e7736c5ae340ad38492
esim <esekenov.e05@gmail.com>
2024-11-21 12:09:02 +0500

    script.js

--- js/script.js
+++ js/script.js
@@ -190,16 +190,16 @@
 // }
 // alert(num + " дейінгі сандардын қосындысы" + sum);
 
-let number = parseInt(prompt("Сан енгізіңіз:"));
- for (let i = 1; i <= number; i++) { console.log("3ke jane 5ke qaldyksyz bolinetin san: 15")
-     if (i % 3 === 0 && i % 5 === 0) {
-         console.log(i); 
-         if (i % 2 === 0) {
-             console.log("3-ке және 5-ке қалдықсыз бөлінетін сан: "+i);
-             break; 
-         }console.log("30 саны 3-ле жане 5-ле болынеды жанеде ол жуп сан => break!");
-     } 
- }
+// let number = parseInt(prompt("Сан енгізіңіз:"));
+//  for (let i = 1; i <= number; i++) { console.log("3ke jane 5ke qaldyksyz bolinetin san: 15")
+//      if (i % 3 === 0 && i % 5 === 0) {
+//          console.log(i); 
+//          if (i % 2 === 0) {
+//              console.log("3-ке және 5-ке қалдықсыз бөлінетін сан: "+i);
+//              break; 
+//          }console.log("30 саны 3-ле жане 5-ле болынеды жанеде ол жуп сан => break!");
+//      } 
+//  }
 
 // let a = +prompt("сан енгізіңіз")
 //     for (let i = 1; i <= a; i++) {
@@ -211,3 +211,379 @@ let number = parseInt(prompt("Сан енгізіңіз:"));
 //         break;
 //         } 
 //     }console.log("30 саны 3-ке және 5-ке бөленеді жәнеде ол жұп сан =>  BREAK!");
+
+
+// let i=0;
+
+// while(i<10){
+//     if(i==5){
+//     console.log("цикл токтатылды,cан табылады: " + i);
+//     break;
+//     }
+//     console.log(i);
+//       i++;
+// }
+
+
+// let num = +prompt("кез келген сан жазыныз")
+//  let sum=0;
+
+//  for (let i = 1; i<=num; i++){
+//     sum = sum + 1;
+//     console.log("sum бізде қазір->" + sum);
+//  }
+//  alert(num + " дейінгі сандардын қосындысы" + sum);
+
+//  let san = +prompt("san engiz")
+
+//  let qosyndy=0;
+//  let i=1;
+
+//  while(i<=san){
+//      qosyndy = qosyndy +i;
+//      i++
+//  }
+
+//  alert(san +"deingi sandar qosyndysy; " + qosyndy )
+
+// let num = +prompt("кез келген сан жазыныз");
+// let sum = 0;
+// let i = 1;
+
+// while (i <= num) {
+//     sum = sum + 1;
+//     console.log("sum бізде қазір->" + sum);
+//     i++;
+// }
+
+// alert(num + " дейінгі сандардын қосындысы " + sum);
+
+// let num = +prompt("кез келген сан жазыныз");
+// let sum = 0;
+// let i = 10;
+
+// while(i <=40 ){
+//     if(i >= 20 && i <= 30 && i % 2 === 0){
+//         i++;
+//         continue;
+//     }
+//     console.log(i)
+//     i++;
+// }
+
+// for(let i=10; i<=40; i++){
+//     if(>=20 && i<=30 && i%2==0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let soz = prompt("qazaq handygy qawan quryldy?")
+// let chance = 3
+
+// for( let i =chance;i>0;1--){
+//     if(soz == "1465")
+//         alert(" correct answer")
+// }
+// else("correst answer")
+// if("wrong answer")
+
+// function salember(habarlama){
+//     alert(habarlama)
+//     console.log(san);
+// }
+
+// qosuAmaly(3,5);
+
+// console.log(qosuAmaly(3,5))
+
+// let kobeitu = qosuamaly(3, 5) * azaituAmaly(5, 3)
+// alert(kobeitu)
+
+// function qosuAmaly(san1,san2){
+//     // console.log(san1+san2); 
+//     return san1 + san2
+// }
+
+// function azaituAmaly(num1, num2){
+//     return num1 - num2
+// }
+
+// let qosasyn = function (number1, number2){
+//     return number1 + number2
+// }
+// let kobeitu = qosuAmaly(3, 5) * azaituamaly(5, 3) * qosasyn(8, 5)
+// //kobeitu = nan * 2
+// alert(kobeitu)//16
+
+// function qosuAmaly(san1,san2){
+//     console.log(san1+san2)// 3 + 5= 8
+//     return san1 + san2
+// }
+
+// function azaituAmaly(num1, num2){ // 5, 3
+//     return num1 - num2
+// }
+
+
+
+
+// let azaitu = function(san1, san2){
+//     console.log("eki sannin azaitindidisi: " +( san1 - san2))
+//     return san1 - san2
+// }
+
+
+// let natizhe = qosu(10,5) + azaitu(6,3) + bolu(4,2)
+// alert("jauab"+natizhe )
+
+
+// function qosu ( san1, san2){
+//     console.log("eki sannin qosindisi: " +(san1 + san2));
+//     return san1 + san2
+// }
+
+// function bolu(san1, san2){
+//     console.log("eki sannin bolindisi: " +(san1/san2));
+//     return san1/san2
+// }
+
+// let name = prompt("atyndy jaz suka")
+
+// function hello(аты){
+//     return "hello, " + аты + "!"
+// }
+// alert(hello(name))
+
+// let san1 = + prompt("san-1 engiz")
+// let san2 = + prompt("san-2 engiz")
+// let san3 = + prompt("san-3 engiz")
+
+// function kobeitu( san1, san2, san3){
+//  return san1 * san2 * san3;
+// }
+
+// alert(kobeitu(san1, san2, san3))
+
+// function bolu(san1, san2){
+//     return san1 / san2
+// }
+
+// let bolingenMan = bolu(15,5)
+
+// console.log(bolingenMan);
+// function kobeitu(san1, san2){
+//     return san1 * san2
+// }
+// let kobeituAmaly = kobeitu(bolu(15,5),2)
+
+// console.log(kobeituAmaly);
+
+
+
+// let qosu =(san1, san2) => san1 + san2;
+// let azaitu =(san1, san2) => san1 - san2;
+// let kobeitu =(san1, san2) => san1 * san2;
+
+// let kobeituNatizhe = kobeitu(qosu(11,5), azaitu(5,3)) 
+
+// alert(kobeituNatizhe)
+
+// let suma = prompt("сума жазыныз")
+// let valuta =+ prompt("валюта танданыз,dollar,evro")
+// let dollar = 496;
+// let evro = 526;
+// function san(suma,valita){
+//      if(valuta =="dollar"){
+//          return suma * 496; 
+
+//      } else if(valuta == "evro"){
+//          return suma * 526;
+
+//      }else{
+//          alert("error")
+//      }
+//  }
+//  alert(euro(valuta,suma));
+
+// let number = +prompt("suma")
+// let valuta = prompt("valuta turin engiz: dollar/euro")
+
+// function converterValut(aksha, valuta){
+//     if(valuta == "dollar"){
+//         return aksha * 496
+//     }else if(valuta == "euro"){
+//         return aksha * 526
+//     }else{
+//         alert("error")
+//     }
+// }
+
+// let converter = converterValut(number, valuta);
+
+// alert(`${number} ${valuta} = ${converter} tenge`)
+
+
+// let san = +prompt("san zhaz cm => mm")
+
+// function converterCmtoMm(number){
+//     return number * 10
+// }
+
+// let converter = converterCmtomm(san);
+
+// alert('$(san) cm = ${converter} mm')
+
+
+// let san = +prompt("san engiz")
+
+// function factorial(number){
+//     if(number<0){
+//         alert("siz 0-den tomen san engizdiniz")
+//     }else if(number == 0 , number == 1){
+//         return 1;
+//     }else{
+//         let i = 2;
+//         let factorial = 1;
+
+//         while(i<=number){
+//             factorial = factorial * i;
+//             i++
+//         }
+//     }
+//     return factorial;
+// }
+// alert(`${san} factorial = ${findfactorial(san)}`)
+
+// document.getElementById("give-alert")
+// .addEventListener("click",()=> alert("hello"))   
+
+
+// let compareScore = (score1,score2)=>{
+//    if(score1>score2){
+//     return " brinshi san ulken"
+//    }else{
+//     return "ekinshi student ulken"
+//    }
+// }
+
+// console.log(compareScore(85,90));
+
+// //task 1
+
+// let qosu = (a,b) =>a+b;
+// console.log(qosu(5,6));
+
+
+// //task 2
+
+// let findleght = (Text)=> Text.length;
+// console.log(findleght("hello, world   !"));
+
+
+// let sum = 0 
+
+// let tagam;
+
+// let max = 0;
+
+// function shygynEsep(){
+//     let i=1;
+//     while(true){
+//         let shygynAty = prompt(i+"-shygyn nomeri aty")
+//         if(shygynAty.toLowerCase() == "аяктау"){
+//             console.log(`shygyn nomeri #${i}: аяктау`);
+//             console.log(`ketken summa: ${sum}`);
+//             console.log(`en qymbat shygyn: ${tagam}`);
+//             break;
+//         } else{
+//             let shygynBaga = +prompt(i+"-shygyn nomerinin bagasy")
+//             console.log(`shygyn nomeri #${i}: ${shygynAty} ${shygynBaga}`);
+//             sum = sum +shygynBaga;
+//             if(max<shygynBaga){
+//                 max=shygynBaga;
+//                 tagam = shygynAty;
+//             }
+//         }
+//         i++;
+//     }
+// }
+
+// shygynEsep();
+
+
+
+// let san = prompt("1 ден 100ге дейын сан жазыныз")
+// switch(san){
+    //     case "1":
+    //         alert("сіз 1 деген санды еңгіздініз")
+    //         break;
+    //     case"2":
+    //      alert("сіз  2 деген санды енгіздініз")
+    //      break;
+    //      case 3:
+    //         alert("сіз 3 деген санды енгіздініз ")
+    //         break;
+    //      default:
+    //             alert("сізқате тердіңіз қайта енгізуді өтінеміз")    
+    // }
+  
+    
+
+
+
+
+
+
+    // let num = 30;
+
+ 
+    // function sandar() {
+    //     while(true){
+    //         let san = +prompt("Сан енгізіңіз: ")
+    //     if(num == san){
+    //       alert("Керемет сіз санды таптыңыз!")
+    //     }else if(san < 0 || san > 100){
+    //         alert("Қүып кеттіңіз!")
+    //     }else if (num-10<=san && san<=num+10){
+    //         alert("Сіз жақындап қалдыңыз: " )
+    //     }else{
+    //         alert("Сіз алыстап кеттіңіз! ")
+    //     }
+    //     }
+    
+    // }
+    
+    
+    // sandar();
+
+
+
+
+
+    document.getElementById("give-alert").addEventListener("click", function(){
+        alert("nice to meet you")
+    })
+
+
+// let number = +prompt("Санды жаз")
+// let max, min;
+// for(let i=1; i<=number; i++){
+//     if(i%5==0){
+//         if(i<min || !max){
+//             min=i
+//         }
+//         if(i>max || !max){
+//             max=i
+//         }
+//     }
+// }
+// if(min && max){
+//     alert("Макс. сан:   " + max)
+//     alert("Мин. сан:   " + min)
+// }
+// else{
+//     console.log("Сан табылмады")
+// }
+
+
